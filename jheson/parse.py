820c9b586e8bad200723de39c100312d23d13de3
abysspacer <artoriastheprogrammer@gmail.com>
2024-08-18 01:48:23 +0330

    add string detection

--- jheson/parse.py
+++ jheson/parse.py
@@ -3,12 +3,31 @@ from utils import get_ready_to_parse_data
 def tokenize_data(data):
     section_starter = ["{", "["]
     section_ender = ["}", "]"]
+    seperator = [":", ","]
 
     def tokenize(string): 
         if string == "": return []
         tkn_lst = []
+
+        # check for string
+        if "\"" in string:
+            first_quote_index = string.index("\"")
+            sub_string_one = string[0: first_quote_index]
+            for tkn in tokenize(sub_string_one):
+                tkn_lst.append(tkn)
+            if "\"" in string[first_quote_index + 1:]:
+                second_quote_index = first_quote_index + string[first_quote_index + 1:].index("\"") + 1
+                tkn_lst.append(string[first_quote_index: second_quote_index + 1])
+                if second_quote_index + 1 < len(string):
+                    sub_string_two = string[second_quote_index + 1:]
+                    for tkn in tokenize(sub_string_two):
+                        tkn_lst.append(tkn)
+            else:
+                tkn_lst.append(string[first_quote_index:])
+            return tkn_lst
+
         # check for section delimiters
-        for indicator in section_starter + section_ender:
+        for indicator in section_starter + section_ender + seperator:
             if indicator in string:
                 # initial split
                 tkns = string.split(indicator)
@@ -36,12 +55,37 @@ def is_valid_json(data):
     section_ender = ["}", "]"]
     
     tokens = tokenize_data(data)
-    
     # basic invalid cases
     if len(tokens) == 0 or (tokens[0] not in section_starter) or (tokens[-1] not in section_ender) or section_ender.index(tokens[-1]) != section_starter.index(tokens[0]): return False
     stack = []
+    statement = []
+    is_comma = False
     for tkn in tokens:
 
+        # string detection
+        if tkn[0] == "'":
+            return False
+        if tkn[0] == "\"":
+            if tkn[-1] != "\"":
+                return False
+            if len(statement) == 0:
+                is_comma = False
+                statement.append(tkn)
+            elif statement[-1] == ":":
+                statement.append(tkn)
+            else:
+                return False
+
+        # seperator detection
+        if tkn == ":":
+            if len(statement) != 1:
+                return False
+            statement.append(tkn)
+        if tkn == ",":
+            if len(statement) != 3:
+                return False
+            statement = []
+            is_comma = True
         # section detection
         if tkn in section_starter:
             stack.append(section_ender[section_starter.index(tkn)])
@@ -49,4 +93,8 @@ def is_valid_json(data):
             if len(stack) == 0 or stack[-1] != tkn:
                 return False
             stack.pop()
+
+    # check for rouge commas
+    if len(statement) == 0 and is_comma:
+        return False
     return True
