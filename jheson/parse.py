c56861c08ee93a719ffc6881b49fc8dc38fd40b2
abysspacer <artoriastheprogrammer@gmail.com>
2024-08-18 05:17:23 +0330

    add array detection

--- jheson/parse.py
+++ jheson/parse.py
@@ -64,55 +64,132 @@ def is_valid_json(data):
     tokens = tokenize_data(data)
     # basic invalid cases
     if len(tokens) == 0 or (tokens[0] not in section_starter) or (tokens[-1] not in section_ender) or section_ender.index(tokens[-1]) != section_starter.index(tokens[0]): return False
+
+    # lexical characteristic
+    # 1 => object
+    # 2 => array
+    lex_char = 1 if tokens[0] == "{" else 2
     stack = []
     statement = []
     is_comma = False
-    for tkn in tokens:
+    i = 1
+    while i < len(tokens) - 1:
+        tkn = tokens[i]
+        # in object
+        if lex_char == 1:
 
-        # string detection
-        if tkn[0] == "'":
-            return False
-        elif tkn[0] == "\"":
-            if tkn[-1] != "\"":
+            # string detection
+            if tkn[0] == "'":
                 return False
-            if len(statement) == 0:
-                is_comma = False
+            elif tkn[0] == "\"":
+                if tkn[-1] != "\"":
+                    return False
+                if len(statement) == 0:
+                    is_comma = False
+                    statement.append(tkn)
+                elif statement[-1] == ":":
+                    statement.append(tkn)
+                else:
+                    return False
+
+            # seperator detection
+            elif tkn == ":":
+                if len(statement) != 1:
+                    return False
                 statement.append(tkn)
-            elif statement[-1] == ":":
+            elif tkn == ",":
+                if len(statement) != 3:
+                    return False
+                statement = []
+                is_comma = True
+
+            # section detection
+            elif tkn in section_starter:
+                if not len(statement) or statement[-1] != ":":
+                    return False
+                statement.append(True)
+                layer_stack_height = len(stack)
+                for j in range(i, len(tokens)):
+                    if tokens[j] in section_starter:
+                        stack.append(section_ender[section_starter.index(tokens[j])])
+                    elif tokens[j] in section_ender:
+                        if len(stack) == 0 or stack[-1] != tokens[j]:
+                            return False
+                        stack.pop()
+                        if len(stack) == layer_stack_height:
+                            if not is_valid_json(" ".join(tokens[i: j + 1])):
+                                return False
+                            i = j
+                            break
+            elif tkn in section_ender:
+                if len(stack) == 0 or stack[-1] != tkn:
+                    return False
+                stack.pop()
+
+            # boolean and number
+            elif tkn == "true" or tkn == "false" or tkn == "null"  or is_number(tkn):
+                if len(statement) != 2:
+                    return False
                 statement.append(tkn)
             else:
                 return False
-
-        # seperator detection
-        elif tkn == ":":
-            if len(statement) != 1:
-                return False
-            statement.append(tkn)
-        elif tkn == ",":
-            if len(statement) != 3:
-                return False
-            statement = []
-            is_comma = True
-        # section detection
-        elif tkn in section_starter:
-            stack.append(section_ender[section_starter.index(tkn)])
-        elif tkn in section_ender:
-            if len(stack) == 0 or stack[-1] != tkn:
-                return False
-            stack.pop()
-
-        # boolean and number
-        elif tkn == "true" or tkn == "false" or tkn == "null"  or is_number(tkn):
-            if len(statement) != 2:
-                return False
-            statement.append(tkn)
+        
+        # in array
         else:
-            return False
+            # string detection
+            if tkn[0] == "'":
+                return False
+            elif tkn[0] == "\"":
+                if tkn[-1] != "\"":
+                    return False
+                if len(statement) == 0:
+                    is_comma = False
+                    statement.append(tkn)
+                else:
+                    return False
 
+            # seperator detection
+            elif tkn == ":":
+                return False
+            elif tkn == ",":
+                if len(statement) != 1:
+                    return False
+                statement = []
+                is_comma = True
 
+            # section detection
+            elif tkn in section_starter:
+                statement.append(True)
+                layer_stack_height = len(stack)
+                stack.append(section_ender[section_starter.index(tkn)])
+                for j in range(i, len(tokens)):
+                    if tokens[j] in section_starter:
+                        stack.append(section_ender[section_starter.index(tkn)])
+                    elif tkn in section_ender:
+                        if len(stack) == 0 or stack[-1] != tkn:
+                            return False
+                        stack.pop()
+                        if len(stack) == layer_stack_height:
+                            if not is_valid_json(" ".join(tokens[i, j + 1])): 
+                                return False
+                            i = j
+                            break
+            elif tkn in section_ender:
+                if len(stack) == 0 or stack[-1] != tkn:
+                    return False
+                stack.pop()
+
+            # boolean and number
+            elif tkn == "true" or tkn == "false" or tkn == "null"  or is_number(tkn):
+                if len(statement) != 0:
+                    return False
+                statement.append(tkn)
+            else:
+                return False
+        i += 1
     # check for rouge commas
     if len(statement) == 0 and is_comma:
         return False
     return True
 
-print(is_valid_json("tests/step3/valid.json"))
+print(is_valid_json("tests/step4/valid2.json"))
